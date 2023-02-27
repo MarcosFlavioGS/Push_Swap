edee41634a07a7d79ccc326face5d03e7b28305b
Marcos Flavio <mfghost69@gmail.com>
2023-02-26 22:50:39 -0300

    Adjusting printing

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ static void	print_linkdlst(t_node *head)
 	current = head;
 	while (current)
 	{
-		ft_printf("%d\n", current->x);
+		ft_printf("Value: %d\n", current->x);
 		ft_printf("Index: %d\n", current->index);
 		ft_printf("Pos: %d\n", current->pos);
 		ft_printf("|\nv\n");
