fb3263b52633a1cc9f1d4a7d445b70e608648682
MarcosFlavioGS <mfghost69@gmail.com>
2023-03-01 20:52:55 -0300

    Resetted sort_bigger()

--- sort_bigger.c
+++ sort_bigger.c
@@ -12,28 +12,6 @@
 
 #include "push_swap.h"
 
-void	pb(t_node **head_b, t_node *new)
-{
-	if (!(*new))
-		return;
-	if (!(*head_b))
-		*head_b = (t_node *) malloc(sizeof(t_node));
-	new->next = *head_b;
-	*head_b = new;
-}
-
-void	pb_all_to_b(t_node **head_a, t_node *head_b)
-{
-	t_node	*curr;
-
-	curr = head_a;
-	while (a)
-	{
-		pb(&head_b, *head_a);
-		curr = curr->next;
-	}
-}
-
 void	sort_bigger(t_node **head_a)
 {
 	t_node	*head_b;
